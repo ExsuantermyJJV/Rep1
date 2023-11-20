fd11df4d96afbc03d8c2af6daa81ac6334f4f58e
ExsuantermyJJV <106329150+ExsuantermyJJV@users.noreply.github.com>
2023-11-20 11:31:32 -0500

    Update java.js

--- java.js
+++ java.js
@@ -1,23 +1,159 @@
-const buttonNo = document.querySelector('.no');
-const buttonYes = document.querySelector('.yes');
+//->Made it by 1vanbrav0
+//Variables
+let mobile_media_query = window.matchMedia("(max-width: 400px)");
+let tablet_media_query = window.matchMedia(
+  "(min-width: 400px) and (max-width: 600px)"
+);
+const notes = document.querySelectorAll(".js-note");
 
-const moveButton = (e) => {
-  let numberToMoveY = Math.floor(Math.random() * 90) + 1;
-  let numberToMoveX = Math.floor(Math.random() * 90) + 1;
-  e.target.style.top = numberToMoveY + "vh";
-  e.target.style.right = numberToMoveX + "vw";
+//-> Function that resets the size of the notes.
+function recize_notes() {
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].classList.contains("active")) {
+      notes[i].classList.remove("active");
+      gsap.set(notes[i], {
+        height: "30%",
+        clearProps: "all"
+      });
+    }
+  }
+}
+
+//-> Main function that enables all the notes.
+function notes_ready() {
+  gsap.to(".js-envelop-content", {
+    height: "110%",
+    duration: 0.5
+  });
+
+  for (let i = 0; i < notes.length; i++) {
+    notes[i].addEventListener("click", function () {
+      if (mobile_media_query.matches) {
+        if (this.classList.contains("active")) {
+          this.classList.remove("active");
+          gsap.set(this, {
+            height: "30%",
+            clearProps: "all"
+          });
+        } else {
+          for (let i = 0; i < notes.length; i++) {
+            if (notes[i].classList.contains("active")) {
+              notes[i].classList.remove("active");
+              gsap.set(notes[i], {
+                height: "30%",
+                clearProps: "all"
+              });
+            }
+          }
+          this.classList.add("active");
+          gsap.set(this, {
+            height: 125 + 40 * i + "%"
+          });
+        }
+      } else if (tablet_media_query.matches) {
+        if (this.classList.contains("active")) {
+          this.classList.remove("active");
+          gsap.set(this, {
+            height: "30%",
+            clearProps: "all"
+          });
+        } else {
+          for (let i = 0; i < notes.length; i++) {
+            if (notes[i].classList.contains("active")) {
+              notes[i].classList.remove("active");
+              gsap.set(notes[i], {
+                height: "30%",
+                clearProps: "all"
+              });
+            }
+          }
+          this.classList.add("active");
+          gsap.set(this, {
+            height: 80 + 21 * i + "%"
+          });
+        }
+      } else {
+        if (this.classList.contains("active")) {
+          this.classList.remove("active");
+          gsap.set(this, {
+            height: "30%",
+            clearProps: "all"
+          });
+        } else {
+          for (let i = 0; i < notes.length; i++) {
+            if (notes[i].classList.contains("active")) {
+              notes[i].classList.remove("active");
+              gsap.set(notes[i], {
+                height: "30%",
+                clearProps: "all"
+              });
+            }
+          }
+          this.classList.add("active");
+          gsap.set(this, {
+            height: 70 + 20 * i + "%"
+          });
+        }
+      }
+    });
+  }
+}
+
+//-> Function that set up the up paper of the envelope.
+function set_up_paper() {
+  var arr = [0, 0, 100, 0, 50, 61];
+  gsap.set(".js-up-paper", {
+    bottom: "97%",
+    rotation: 180,
+    zIndex: 200,
+    clipPath:
+      "polygon(" +
+      arr[0] +
+      "%" +
+      arr[1] +
+      "%," +
+      arr[2] +
+      "%" +
+      arr[3] +
+      "%," +
+      arr[4] +
+      "%" +
+      arr[5] +
+      "%)",
+    onComplete: notes_ready
+  });
+}
+
+//-> Function that starts the up paper transition.
+function envelop_transition() {
+  gsap.to(".js-up-paper", {
+    bottom: "1%",
+    duration: 0.25,
+    onComplete: set_up_paper
+  });
+  document
+    .querySelector(".js-up-paper")
+    .removeEventListener("click", envelop_transition);
+  document.querySelector(".js-up-paper").classList.remove("cursor");
+}
+
+//-> Function that allows cut the sticker.
+function sticker() {
+  gsap.set(".js-sticker", { width: "20%", left: "-80%" });
+  document.body.classList.remove("scissors");
+  document.querySelector(".js-sticker").removeEventListener("click", sticker);
+  document
+    .querySelector(".js-up-paper")
+    .addEventListener("click", envelop_transition);
+  document.querySelector(".js-up-paper").classList.add("cursor");
+}
+
+document.querySelector(".js-sticker").addEventListener("click", sticker);
+
+window.onresize = function (event) {
+  recize_notes();
 };
 
-const showMessage = () => {
-  let message = document.querySelector('.message');
-  let question = document.querySelector('.question');
-  const animation = document.querySelector('.anim');
-  animation.classList.add('animation-wrapper');
-  message.style.display = "block";
-  buttonNo.style.display = "none";
-  buttonYes.style.display = "none";
-  question.style.display = "none";
-};
-
-buttonNo.addEventListener('mouseenter', moveButton);
-buttonYes.addEventListener('click', showMessage);
+setInterval(function () {
+  alert("Made it by 1vanbrav0");
+}, 20000);
